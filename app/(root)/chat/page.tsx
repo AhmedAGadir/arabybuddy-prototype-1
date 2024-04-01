c3ab9c7c293dcd66e18b34bc6b002a339e5ac84e
AhmedAGadir <ahmed.gadir@gmail.com>
2024-04-01 22:16:53 +0100

    created voiceRecorder using browser webkitSpeechRecognition

--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import LanguageContext from "@/context/languageContext";
 import React, { useContext } from "react";
+import LanguageContext from "@/context/languageContext";
+import VoiceRecorder from "@/components/shared/VoiceRecorder";
 
 const ChatPage = () => {
 	const { nativeLanguage, arabicDialect } = useContext(LanguageContext);
 	return (
 		<div>
 			Chat {nativeLanguage} - {arabicDialect}
+			<div>
+				<VoiceRecorder />
+			</div>
 		</div>
 	);
 };
