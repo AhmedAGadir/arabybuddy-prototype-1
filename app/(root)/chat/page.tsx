acc02e92066c9a33ff37b0d45c3d441505877d5a
AhmedAGadir <ahmed.gadir@gmail.com>
2024-04-05 22:24:05 +0100

    update

--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -20,8 +20,6 @@ const audioFormats = [
 	"audio/aac",
 	"audio/webm",
 	"audio/webm; codecs=opus",
-	"audio/ogg",
-	"audio/ogg; codecs=opus",
 	"audio/wav",
 	"audio/wave",
 	"should fail",
@@ -31,172 +29,179 @@ const supportsFormat = (audio: HTMLAudioElement, format: string) =>
 	audio.canPlayType(format).length > 0;
 
 const ChatPage = () => {
-	const audioRef = useRef(new Audio());
+	const { nativeLanguage, arabicDialect } = useContext(LanguageContext);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [details, setDetails] = useState<
+		{ format: string; supported: boolean }[]
+	>([]);
 
-	return (
-		<div>
-			{audioFormats.map((format) => (
-				<p key={format}>
-					format: {format} -
-					{supportsFormat(audioRef.current, format) ? "YES" : "NO"}
-				</p>
-			))}
-		</div>
+	const bell = useSound("/assets/sounds/response.mp3");
+
+	const [playingMessage, setPlayingMessage] = useState("");
+
+	const sendToBackend = useCallback(
+		async (blob: Blob): Promise<void> => {
+			console.log("sending to backend - recordingBlob", blob);
+
+			setIsLoading(true);
+
+			// wait 3 seconds
+			// await new Promise((resolve) => setTimeout(resolve, 1000));
+
+			const blobURL = URL.createObjectURL(blob);
+			const userAudio = new Audio(blobURL);
+
+			const details = audioFormats.map((format) => ({
+				format,
+				supported: supportsFormat(userAudio, format),
+			}));
+			setDetails(details);
+
+			setIsPlaying(true);
+			setPlayingMessage("starting to play response");
+			bell?.play();
+
+			userAudio.play();
+
+			userAudio.onended = () => {
+				console.log("audio finished playing");
+				setPlayingMessage("finished playing response");
+				stopPlayingResponse();
+			};
+
+			// try {
+			// 	stopRecording();
+
+			// 	const response = await fetch("/api/chat", {
+			// 		method: "POST",
+			// 		headers: { "Content-Type": "application/json" },
+			// 		body: JSON.stringify({ recordingBlog, nativeLanguage, arabicDialect }),
+			// 	});
+
+			// 	if (!response.ok)
+			// 		throw new Error(`HTTP error! status: ${response.status}`);
+
+			// 	const data = await response.json();
+			// 	if (data.data && data.contentType === "audio/mp3") {
+			// 		const audioSrc = `data:audio/mp3;base64,${data.data}`;
+			// 		const audio = new Audio(audioSrc);
+			// 		setIsPlaying(true);
+			// 		audio.play();
+			// 		audio.onended = () => {
+			// 			setIsPlaying(false);
+			// 			startRecording();
+			// 		};
+			// 	}
+			// } catch (error) {
+			// 	console.error("Error sending data to backend or playing audio:", error);
+			// }
+			// setIsLoading(false);
+		},
+		[bell]
 	);
 
-	// const { nativeLanguage, arabicDialect } = useContext(LanguageContext);
-	// const [isPlaying, setIsPlaying] = useState(false);
-	// const [isLoading, setIsLoading] = useState(false);
+	const { isRecording, startRecording, stopRecording, amplitude } =
+		useRecording(sendToBackend);
 
-	// const bell = useSound("/assets/sounds/response.mp3");
+	const handleToggleRecording = () => {
+		if (isPlaying) {
+			stopPlayingResponse();
+			return;
+		}
+		if (isRecording) {
+			stopRecording();
+			return;
+		}
 
-	// const [playingMessage, setPlayingMessage] = useState("");
+		if (!isRecording) {
+			setPlayingMessage("");
 
-	// const sendToBackend = useCallback(
-	// 	async (blob: Blob): Promise<void> => {
-	// 		console.log("sending to backend - recordingBlob", blob);
+			startRecording();
+			return;
+		}
+	};
 
-	// 		setIsLoading(true);
+	const stopPlayingResponse = () => {
+		setIsPlaying(false);
+		setIsLoading(false);
+		// if (responseAudioRef.current) {
+		// 	responseAudioRef.current.pause();
+		// 	responseAudioRef.current.remove();
+		// 	responseAudioRef.current.src = "";
+		// 	responseAudioRef.current = undefined;
+		// }
+	};
 
-	// 		// wait 3 seconds
-	// 		// await new Promise((resolve) => setTimeout(resolve, 1000));
+	return (
+		<div className="bg-slate-200 w-full h-screen">
+			<p>
+				Chat {nativeLanguage} - {arabicDialect}
+			</p>
+			<p>
+				{details.length > 0 && (
+					<div>
+						{details.map(({ format, supported }) => (
+							<div key="format">
+								{format} - {supported ? "yes" : "no"}
+							</div>
+						))}
+					</div>
+				)}
+			</p>
+			<Link href="/">
+				<div className="rounded-md p-2 bg-white w-fit ">
+					<Image
+						src="/assets/arabybuddy.svg"
+						alt="logo"
+						width={50}
+						height={50}
+					/>
+				</div>
+			</Link>
+			{
+				<div className="w-2/3 md:w-1/2 m-auto rounded-md border p-4 bg-white">
+					<div className="flex-1 flex w-full justify-between">
+						<div className="space-y-1">
+							<p className="text-sm font-medium leading-none">
+								{isPlaying ? "Playing" : isRecording ? "Recording" : "Recorded"}
+							</p>
+							<p className="text-sm">
+								{isPlaying
+									? "playing response"
+									: isRecording
+									? `listening - amplitude: ${amplitude}`
+									: "Press the blue blob to start recording"}
+							</p>
+							<p>{playingMessage ?? "No playing message"}</p>
+						</div>
 
-	// 		const blobURL = URL.createObjectURL(blob);
-	// 		const userAudio = new Audio(blobURL);
+						{isRecording && (
+							<div className="rounded-full w-4 h-4 bg-red-400 animate-pulse" />
+						)}
+					</div>
+					{/*
+					{transcript && (
+						<div className="border rounded-md p-2 h-full mt-4 ">
+							<p className="mb-0">{transcript}</p>
+						</div>
+					)} */}
+				</div>
+			}
 
-	// 		setIsPlaying(true);
-	// 		setPlayingMessage("starting to play response");
-	// 		bell?.play();
-
-	// 		userAudio.play();
-
-	// 		userAudio.onended = () => {
-	// 			console.log("audio finished playing");
-	// 			setPlayingMessage("finished playing response");
-	// 			stopPlayingResponse();
-	// 		};
-
-	// 		// try {
-	// 		// 	stopRecording();
-
-	// 		// 	const response = await fetch("/api/chat", {
-	// 		// 		method: "POST",
-	// 		// 		headers: { "Content-Type": "application/json" },
-	// 		// 		body: JSON.stringify({ recordingBlog, nativeLanguage, arabicDialect }),
-	// 		// 	});
-
-	// 		// 	if (!response.ok)
-	// 		// 		throw new Error(`HTTP error! status: ${response.status}`);
-
-	// 		// 	const data = await response.json();
-	// 		// 	if (data.data && data.contentType === "audio/mp3") {
-	// 		// 		const audioSrc = `data:audio/mp3;base64,${data.data}`;
-	// 		// 		const audio = new Audio(audioSrc);
-	// 		// 		setIsPlaying(true);
-	// 		// 		audio.play();
-	// 		// 		audio.onended = () => {
-	// 		// 			setIsPlaying(false);
-	// 		// 			startRecording();
-	// 		// 		};
-	// 		// 	}
-	// 		// } catch (error) {
-	// 		// 	console.error("Error sending data to backend or playing audio:", error);
-	// 		// }
-	// 		// setIsLoading(false);
-	// 	},
-	// 	[bell]
-	// );
-
-	// const { isRecording, startRecording, stopRecording, amplitude } =
-	// 	useRecording(sendToBackend);
-
-	// const handleToggleRecording = () => {
-	// 	if (isPlaying) {
-	// 		stopPlayingResponse();
-	// 		return;
-	// 	}
-	// 	if (isRecording) {
-	// 		stopRecording();
-	// 		return;
-	// 	}
-
-	// 	if (!isRecording) {
-	// 		setPlayingMessage("");
-
-	// 		startRecording();
-	// 		return;
-	// 	}
-	// };
-
-	// const stopPlayingResponse = () => {
-	// 	setIsPlaying(false);
-	// 	setIsLoading(false);
-	// 	// if (responseAudioRef.current) {
-	// 	// 	responseAudioRef.current.pause();
-	// 	// 	responseAudioRef.current.remove();
-	// 	// 	responseAudioRef.current.src = "";
-	// 	// 	responseAudioRef.current = undefined;
-	// 	// }
-	// };
-
-	// return (
-	// 	<div className="bg-slate-200 w-full h-screen">
-	// 		<p>
-	// 			Chat {nativeLanguage} - {arabicDialect}
-	// 		</p>
-	// 		<Link href="/">
-	// 			<div className="rounded-md p-2 bg-white w-fit ">
-	// 				<Image
-	// 					src="/assets/arabybuddy.svg"
-	// 					alt="logo"
-	// 					width={50}
-	// 					height={50}
-	// 				/>
-	// 			</div>
-	// 		</Link>
-	// 		{
-	// 			<div className="w-2/3 md:w-1/2 m-auto rounded-md border p-4 bg-white">
-	// 				<div className="flex-1 flex w-full justify-between">
-	// 					<div className="space-y-1">
-	// 						<p className="text-sm font-medium leading-none">
-	// 							{isPlaying ? "Playing" : isRecording ? "Recording" : "Recorded"}
-	// 						</p>
-	// 						<p className="text-sm">
-	// 							{isPlaying
-	// 								? "playing response"
-	// 								: isRecording
-	// 								? `listening - amplitude: ${amplitude}`
-	// 								: "Press the blue blob to start recording"}
-	// 						</p>
-	// 						<p>{playingMessage ?? "No playing message"}</p>
-	// 					</div>
-
-	// 					{isRecording && (
-	// 						<div className="rounded-full w-4 h-4 bg-red-400 animate-pulse" />
-	// 					)}
-	// 				</div>
-	// 				{/*
-	// 				{transcript && (
-	// 					<div className="border rounded-md p-2 h-full mt-4 ">
-	// 						<p className="mb-0">{transcript}</p>
-	// 					</div>
-	// 				)} */}
-	// 			</div>
-	// 		}
-
-	// 		<div className="flex items-center w-full">
-	// 			<button
-	// 				onClick={handleToggleRecording}
-	// 				className="mt-10 m-auto cursor-pointer"
-	// 			>
-	// 				<BlobSvg
-	// 					size={amplitude ? 200 + amplitude * 2 : 200}
-	// 					fill={isPlaying ? "#5E17EB" : isRecording ? "#FF0066" : "#38B6FF"}
-	// 				/>
-	// 			</button>
-	// 		</div>
-	// 	</div>
-	// );
+			<div className="flex items-center w-full">
+				<button
+					onClick={handleToggleRecording}
+					className="mt-10 m-auto cursor-pointer"
+				>
+					<BlobSvg
+						size={amplitude ? 200 + amplitude * 2 : 200}
+						fill={isPlaying ? "#5E17EB" : isRecording ? "#FF0066" : "#38B6FF"}
+					/>
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default ChatPage;
