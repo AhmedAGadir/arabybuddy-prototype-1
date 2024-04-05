43abe15b2965e1a289ab71149a85dfa3bcab1d1b
AhmedAGadir <ahmed.gadir@gmail.com>
2024-04-05 21:41:15 +0100

    test

--- hooks/useRecording.ts
+++ hooks/useRecording.ts
@@ -1,20 +1,36 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useSilenceDetection } from "./useSilenceDetection";
 import { useSound } from "./useSound";
-import { usePolyfill } from "./usePolyfil";
+import { usePolyfill } from "./useMediaRecorderPolyfil";
 
 const useRecording = (onRecordingComplete?: (blob: Blob) => void) => {
 	const [isRecording, setIsRecording] = useState(false);
+
 	const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+	// const { getMediaRecorder } = usePolyfill();
+	const streamRef = useRef<MediaStream>();
 	const chunksRef = useRef<Blob[]>([]);
 
-	const { getMediaRecorder } = usePolyfill();
+	const { detectSilence, amplitude, stopSilenceDetection } =
+		useSilenceDetection();
 
 	const startSound = useSound("/assets/sounds/start.mp3");
 	const stopSound = useSound("/assets/sounds/stop.mp3");
 
-	const { detectSilence, amplitude, stopSilenceDetection } =
-		useSilenceDetection();
+	useEffect(() => {
+		const requestPermission = async () => {
+			try {
+				const stream = await navigator.mediaDevices.getUserMedia({
+					audio: true,
+				});
+				streamRef.current = stream;
+			} catch (err) {
+				console.error("Failed to get user media", err);
+			}
+		};
+
+		requestPermission();
+	}, []);
 
 	const stopRecording = useCallback(() => {
 		setIsRecording(false);
@@ -25,40 +41,32 @@ const useRecording = (onRecordingComplete?: (blob: Blob) => void) => {
 	}, [stopSilenceDetection, stopSound]);
 
 	const startRecording = useCallback(() => {
-		startSound?.play();
+		if (!streamRef.current) return;
 
+		startSound?.play();
 		setIsRecording(true);
 
-		navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-			mediaRecorderRef.current = getMediaRecorder(stream);
+		mediaRecorderRef.current = new MediaRecorder(streamRef.current);
+		mediaRecorderRef.current.start();
 
-			mediaRecorderRef.current.start();
+		mediaRecorderRef.current.ondataavailable = (e) => {
+			chunksRef.current.push(e.data);
+		};
 
-			mediaRecorderRef.current.ondataavailable = (e) => {
-				chunksRef.current.push(e.data);
-			};
+		mediaRecorderRef.current.onstop = (e) => {
+			const blob = new Blob(chunksRef.current, { type: "audio/mp4" });
+			onRecordingComplete?.(blob);
+			chunksRef.current = [];
 
-			mediaRecorderRef.current.onstop = (e) => {
-				const blob = new Blob(chunksRef.current, { type: "audio/mp4" });
-				onRecordingComplete?.(blob);
-				chunksRef.current = [];
+			if (mediaRecorderRef.current) {
+				mediaRecorderRef.current.ondataavailable = null;
+				mediaRecorderRef.current.onstop = null;
+				mediaRecorderRef.current = null;
+			}
+		};
 
-				if (mediaRecorderRef.current) {
-					mediaRecorderRef.current.ondataavailable = null;
-					mediaRecorderRef.current.onstop = null;
-					mediaRecorderRef.current = null;
-				}
-			};
-
-			detectSilence(mediaRecorderRef.current, 3000, stopRecording);
-		});
-	}, [
-		detectSilence,
-		getMediaRecorder,
-		onRecordingComplete,
-		startSound,
-		stopRecording,
-	]);
+		detectSilence(mediaRecorderRef.current, 3000, stopRecording);
+	}, [detectSilence, onRecordingComplete, startSound, stopRecording]);
 
 	return {
 		isRecording,
