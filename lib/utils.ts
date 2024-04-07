a497f7f1e0c742b8e57a7a50b58cbed6d27663d6
AhmedAGadir <ahmed.gadir@gmail.com>
2024-04-07 02:34:03 +0100

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,3 @@
-"use client";
-
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { MIME_TYPES } from "./constants";
@@ -14,6 +12,7 @@ export const isDefined = <T>(value: T | undefined): value is T =>
 export const isNotNull = <T>(value: T | null): value is T => value !== null;
 
 export const iOS = () => {
+	if (!navigator) return false;
 	return (
 		[
 			"iPad Simulator",
