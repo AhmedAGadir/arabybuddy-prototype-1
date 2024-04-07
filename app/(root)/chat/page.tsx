935574ca997976036a2fdb7703248ece7a990d27
AhmedAGadir <ahmed.gadir@gmail.com>
2024-04-07 02:38:44 +0100

    update

--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -11,18 +11,21 @@ import { useSound } from "@/hooks/useSound";
 import { getFirstSupportedMimeType } from "@/lib/utils";
 
 const iOS = () => {
-	return (
-		[
-			"iPad Simulator",
-			"iPhone Simulator",
-			"iPod Simulator",
-			"iPad",
-			"iPhone",
-			"iPod",
-		].includes(navigator.platform) ||
-		// iPad on iOS 13 detection
-		(navigator.userAgent.includes("Mac") && "ontouchend" in document)
-	);
+	if (typeof window !== "undefined") {
+		return (
+			[
+				"iPad Simulator",
+				"iPhone Simulator",
+				"iPod Simulator",
+				"iPad",
+				"iPhone",
+				"iPod",
+			].includes(navigator.platform) ||
+			// iPad on iOS 13 detection
+			(navigator.userAgent.includes("Mac") && "ontouchend" in document)
+		);
+	}
+	return false;
 };
 
 const ChatPage = () => {
