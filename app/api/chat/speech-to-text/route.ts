0ab77cd3ed725e32a7f428780e49308108200d02
AhmedAGadir <ahmed.gadir@gmail.com>
2024-04-08 19:46:37 +0100

    using tmpdir from os package

--- app/api/chat/speech-to-text/route.ts
+++ app/api/chat/speech-to-text/route.ts
@@ -1,36 +1,18 @@
 import fs from "fs";
 import path from "path";
 import OpenAI from "openai";
+import { tmpdir } from "os";
 
+// remember API calls wont work if the account balance is 0
 const openai = new OpenAI({
-	organization: "org-B14Y5vkrVXQspVLJnhcfWvqD",
+	organization: process.env.OPENAI_ORG_ID,
 	apiKey: process.env.OPENAI_API_KEY,
 });
 
-// export async function POST(req: Request, res: Response) {
-// 	try {
-// 		const transcription = await openai.audio.transcriptions.create({
-// 			file: fs.createReadStream("public/assets/sounds/test-user-audio.mp3"),
-// 			model: "whisper-1",
-// 			response_format: "text",
-// 		});
-
-// 		console.log(transcription);
-
-// 		return Response.json({ result: transcription.text });
-// 	} catch (error) {
-// 		console.error("Error processing audio:", error);
-// 		return Response.error();
-// 	}
-// }
-
 export async function POST(req: Request, res: Response) {
 	try {
-		// Parse the request body and extract the audio data
 		const { base64Audio, type } = await req.json();
-
 		const { transcription } = await openAISpeechToText(base64Audio, type);
-
 		return Response.json({ result: transcription }, { status: 200 });
 	} catch (error) {
 		console.error("Error processing audio:", error);
@@ -39,17 +21,13 @@ export async function POST(req: Request, res: Response) {
 }
 
 const openAISpeechToText = async (base64Audio: string, type: string) => {
-	// Convert the Base64 audio data back to a Buffer
 	const audioData = Buffer.from(base64Audio, "base64");
 
-	// Correctly resolve the path to the temporary directory
-	const dirPath = path.join(process.cwd(), "tmp");
+	// https://github.com/orgs/vercel/discussions/241
+	const dirPath = path.join(tmpdir(), "openai-audio-transcription");
 	const filePath = path.join(dirPath, `input.${type}`);
 
-	// Ensure the directory exists
 	await fs.promises.mkdir(dirPath, { recursive: true });
-
-	// Write the file
 	fs.writeFileSync(filePath, audioData);
 
 	const transcription = await openai.audio.transcriptions.create({
@@ -57,9 +35,7 @@ const openAISpeechToText = async (base64Audio: string, type: string) => {
 		model: "whisper-1",
 	});
 
-	// Remove the file after use
 	fs.unlinkSync(filePath);
-	// Remove the tmp directory
 	fs.rmdirSync(dirPath);
 
 	return { transcription: transcription.text };
