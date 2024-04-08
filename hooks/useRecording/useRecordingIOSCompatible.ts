13dc2f039e3b37cfae9ce31070da673f2571f290
AhmedAGadir <ahmed.gadir@gmail.com>
2024-04-08 23:09:10 +0100

    testing with a new iOS recorder in wav

--- hooks/useRecording/useRecordingIOSCompatible.ts
+++ hooks/useRecording/useRecordingIOSCompatible.ts
@@ -1,8 +1,9 @@
 import { useState, useRef, useCallback } from "react";
-import { useSilenceDetection } from "./useSilenceDetection";
-import { useSound } from "../useSound";
+import { useSilenceDetection } from "@/hooks/useRecording/useSilenceDetection";
+import { useSound } from "@/hooks/useSound";
 import { getFirstSupportedMimeType } from "@/lib/utils";
-import { useRecordingLogger } from "./logger";
+import { useRecordingLogger } from "@/hooks/useRecording/logger";
+import { Recorder } from "@/lib/recorder";
 
 // this version of useRecording IS compatible with iOS
 // however it asks for microphone permission every time the user starts recording
@@ -15,8 +16,9 @@ const useRecordingIOSCompatible = (
 	const isRecordingRef = useRef<boolean>();
 	isRecordingRef.current = isRecording;
 
-	const mediaRecorderRef = useRef<MediaRecorder | null>(null);
-	const streamRef = useRef<MediaStream>();
+	const audioContextRef = useRef<AudioContext | null>(null);
+	const inputRef = useRef<MediaStreamAudioSourceNode | null>(null);
+	const recorderRef = useRef<any>(null);
 
 	const { detectSilence, amplitude, stopSilenceDetection } =
 		useSilenceDetection();
@@ -24,39 +26,35 @@ const useRecordingIOSCompatible = (
 	const startSound = useSound("/assets/sounds/start.mp3");
 	const stopSound = useSound("/assets/sounds/stop.mp3");
 
-	const dataAvailableHandler = useCallback(
-		(event: BlobEvent) => {
-			try {
-				const chunks = event.data;
-				const blob = new Blob([chunks], {
-					type: getFirstSupportedMimeType(),
-				});
-
-				onRecordingComplete?.(blob);
-				stopSilenceDetection();
-			} catch (error) {
-				logger.log(
-					`Failed to handle recorded data: ${JSON.stringify(
-						(error as any).message
-					)}`
-				);
-			}
-		},
-		[logger, onRecordingComplete, stopSilenceDetection]
-	);
-
-	// Function to stop recording
 	const stopRecording = useCallback(() => {
 		if (!isRecordingRef.current) {
 			logger.warn("Recording is not in progress");
 			return;
 		}
 
-		// Stop the media recorder
 		stopSound?.play();
-		mediaRecorderRef.current?.stop();
-		setIsRecording(false);
-	}, [logger, stopSound]);
+		recorderRef.current?.stop();
+		recorderRef.current?.exportWAV((blob: Blob) => {
+			logger.log(`Recording stopped - blob ${blob}`);
+
+			onRecordingComplete(blob);
+			stopSilenceDetection();
+
+			recorderRef.current?.clear();
+			setIsRecording(false);
+
+			// clean up
+			audioContextRef.current?.close();
+			audioContextRef.current = null;
+
+			inputRef.current?.mediaStream.getAudioTracks().forEach((track) => {
+				track.stop();
+			});
+			inputRef.current = null;
+
+			recorderRef.current = null;
+		});
+	}, [logger, onRecordingComplete, stopSilenceDetection, stopSound]);
 
 	const startRecording = useCallback(async () => {
 		if (isRecording) {
@@ -65,51 +63,30 @@ const useRecordingIOSCompatible = (
 		}
 
 		try {
-			// Request access to the media stream (audio and video)
 			const stream = await navigator.mediaDevices.getUserMedia({
 				audio: true,
 			});
-			streamRef.current = stream;
 
-			// Initialize MediaRecorder
-			const mediaRecorder = new MediaRecorder(stream);
-			mediaRecorderRef.current = mediaRecorder;
+			const audioContext = new (window.AudioContext ||
+				//@ts-ignore
+				window.webkitAudioContext)();
+			const input = audioContext.createMediaStreamSource(stream);
+			var recorder = new Recorder(input);
 
-			// Start recording
+			audioContextRef.current = audioContext;
+			inputRef.current = input;
+			recorderRef.current = recorder;
+
 			startSound?.play();
-			mediaRecorder.start();
+			recorder.record();
 			setIsRecording(true);
-			detectSilence(mediaRecorderRef.current, 3000, stopRecording);
-
-			// Handle data available after stopping
-			mediaRecorder.addEventListener("dataavailable", dataAvailableHandler);
-
-			// Handle recording stop
-			mediaRecorder.onstop = () => {
-				// Clean up resources
-				stream.getTracks().forEach((track) => track.stop());
-				streamRef.current = undefined;
-
-				mediaRecorderRef.current?.removeEventListener(
-					"dataavailable",
-					dataAvailableHandler
-				);
-				mediaRecorderRef.current?.stop();
-				mediaRecorderRef.current = null;
-			};
+			// detectSilence(audioContextRef.current, 3000, stopRecording);
 		} catch (error) {
 			logger.log(
 				`Failed to start recording: ${JSON.stringify((error as any).message)}`
 			);
 		}
-	}, [
-		dataAvailableHandler,
-		detectSilence,
-		isRecording,
-		logger,
-		startSound,
-		stopRecording,
-	]);
+	}, [isRecording, logger, startSound]);
 
 	return { isRecording, startRecording, stopRecording, amplitude };
 };
