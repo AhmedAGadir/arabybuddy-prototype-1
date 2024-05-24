23f8421bfc64c161e10a584f93a42126ee9bb632
AhmedAGadir <ahmed.gadir@gmail.com>
2024-05-24 13:36:53 +0100

    update

--- app/(root)/(main)/chat/page.tsx
+++ app/(root)/(main)/chat/page.tsx
@@ -195,7 +195,7 @@ const ChatPage = () => {
 			>
 				<Card
 					className={cn(
-						"w-full sm:w-[300px] bg-background flex flex-col relative transition-all ease-in duration-50 h-full",
+						"w-full md:w-[300px] bg-background flex flex-col relative transition-all ease-in duration-50 h-full",
 						!filtered && "opacity-40 filter blur-md"
 					)}
 				>
